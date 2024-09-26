8afda8076a7d36367220e048b3448be5240f552c
Miriam Mantsurova <marimantsurova@gmail.com>
2024-09-27 02:09:40 +0300

    more fixes..

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { FronteggProvider } from "@frontegg/react";
 const contextOptions = {
   baseUrl: import.meta.env.VITE_API_URL || "",
   clientId: import.meta.env.VITE_CLIENT_ID || "",
-  appId: "732689f4-18cf-4fb9-856a-26bd4485b841",
+  appId: import.meta.env.VITE_APP_ID || "",
 };
 const container = document.getElementById("root");
 
